c6d631edee5145830117f151e02a8e2a0d6ec83c
Wotox2 <MAHDIKAMALY20@GMAIL.COM>
2022-06-18 10:33:43 +0430

    Updated version 1

--- index.php
+++ index.php
@@ -11,51 +11,50 @@
     <title>Main Page</title>
 
 
- 
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-        
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
     <link rel="stylesheet" href=<?= asset('assets/css/main-page/main-page.css') ?>>
     <link rel="stylesheet" href=<?= asset('assets/css/main-page/main-page.css.map') ?>>
 
 
-       <!-- Slick Slider -->
- <link href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" rel="stylesheet" />
- <link href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" rel="stylesheet" />
-       
+    <!-- Slick Slider -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" rel="stylesheet" />
+
 
 </head>
+
 <body class="main-page">
 
- <?php require_once 'layouts/top-nav.php' ?>
-    
+    <?php require_once 'layouts/top-nav.php' ?>
 
- <section class="background-slider">
- <?php
-    global $pdo;
-                    $query = "SELECT `posts`.*, categories.name AS category_name From posts LEFT JOIN categories ON posts.cat_id = categories.id WHERE `status` = 10";
-                    $statement = $pdo->prepare($query);
-                    $statement ->execute();
-                    $posts = $statement->fetchall();
-                    foreach($posts as $post){
-                    ?>
-        <a class="slide" href="<?= url('detail.php?post_id='. $post->id) ?>">
-            <img src=<?= asset($post->image) ?> alt="">
-            <div class="slide-title">
-                <div class="title-header">
-                    <h1><?= $post->title ?></h1>
+
+    <section class="background-slider">
+        <?php
+        global $pdo;
+        $query = "SELECT `posts`.*, categories.name AS category_name From posts LEFT JOIN categories ON posts.cat_id = categories.id WHERE `status` = 10";
+        $statement = $pdo->prepare($query);
+        $statement->execute();
+        $posts = $statement->fetchall();
+        foreach ($posts as $post) {
+        ?>
+            <a class="slide" href="<?= url('detail.php?post_id=' . $post->id) ?>">
+                <img src=<?= asset($post->image) ?> alt="">
+                <div class="slide-title">
+                    <div class="title-header">
+                        <h1><?= $post->title ?></h1>
+                    </div>
+                    <div class="title-body">
+                        تومان<h1><?= $post->price ?></h1>
+                    </div>
                 </div>
-                <div class="title-body">
-                    تومان<h1><?= $post->price ?></h1>
-                </div>
-            </div>
-        </a>
+            </a>
         <?php } ?>
-      
-        
+
+
     </section>
 
     <section class="intro">
@@ -65,7 +64,7 @@
         <div class="right">
             <h1>دیگه منتظر نمون تا خونه رویاییت پیدا بشه </h1>
             <li>با خونه دات کام میتونی خونه رویاییت رو پیدا کنی و با کمترین زمان وارد معامله با آژانس 24 ساعته بشی</li>
-            <li>با سابقه فروش 400 خونه  واقعا شگفت انگیزه</li>
+            <li>با سابقه فروش 400 خونه واقعا شگفت انگیزه</li>
             <li>خونه داری ؟ میتونی تو همینجا بفروشی با کمک آژانس های مترجب ما</li>
             <li>نمیتونی مشتری هاتونو راضی کنید ؟ خوب کاری نداره خونه تون رو با اسم ما بفروشین اینجوری همه بهتون اعتماد دارن</li>
             <li>درصدی از معامله دریافت نمیکنیم برای حق معامله پس خیالت راحت راحت</li>
@@ -75,85 +74,240 @@
 
     <section class="todays-estates">
         <div class="todays-estates-list">
-        <?php
-                    global $pdo;
-                    $query = "SELECT `posts`.*, categories.name AS category_name From posts LEFT JOIN categories ON posts.cat_id = categories.id WHERE `status` = 10 ;";
-                    $statement = $pdo->prepare($query);
-                    $statement ->execute();
-                    $posts = $statement->fetchall();
-                    foreach($posts as $post){
-                       ?>
-        <a class="card click" href=<?= url('detail.php?post_id=' . $post->id) ?>>
-                <div class="card-header">
-                    <div class="card-image card-row">
-                        <img src=<?= asset($post->image) ?> alt="">
+            <?php
+            global $pdo;
+            $query = "SELECT `posts`.*, categories.name AS category_name From posts LEFT JOIN categories ON posts.cat_id = categories.id WHERE `status` = 10 ;";
+            $statement = $pdo->prepare($query);
+            $statement->execute();
+            $posts = $statement->fetchall();
+            foreach ($posts as $post) {
+            ?>
+                <a class="card click" href=<?= url('detail.php?post_id=' . $post->id) ?>>
+                    <div class="card-header">
+                        <div class="card-image card-row">
+                            <img src=<?= asset($post->image) ?> alt="">
+                        </div>
+                        <div class="card-category card-row">
+                            <span><?= $post->category_name ?></span>
+                        </div>
                     </div>
-                    <div class="card-category card-row">
-                       <span><?= $post->category_name ?></span> 
-                    </div>
-                </div>
-                <div class="card-body">
-                    <div class="card-name card-row">
-                        <h1> <?= $post->title ?></h1>
-                    </div>
-                    <div class="card-address card-row">
-                        <p> <?= $post->address ?></p>
-                    </div>
-                    <div class="card-options card-row">
-                        <div class="sqfr option">
-                            <h1>متراژ</h1>
-                            <span><?= $post->area ?></span> متر
+                    <div class="card-body">
+                        <div class="card-name card-row">
+                            <h1> <?= $post->title ?></h1>
                         </div>
-                        <div class="beds option">
-                            <h1>اتاق</h1>
-                            <span><?= $post->rooms ?></span>
+                        <div class="card-address card-row">
+                            <p> <?= $post->address ?></p>
                         </div>
-                        <div class="bath option">
-                            <h1>حمام</h1>
-                            <span><?= $post->shower ?></span>
-                        </div>
-                        <div class="bath option">
-                            <h1>آدرس</h1>
-                            <span><?= $post->address ?></span>
-                        </div>
-                        <div class="balcony option">
-                            <h1>بالکن</h1>
-                            <span><?php  $post->balcony == 10 ? print 'دارد' : print 'ندارد'; ?></span>
-                        </div>
-                        <div class="parking option">
-                            <h1>پارکینگ</h1>
-                            <span><?php   $post->parking == 10 ? print 'دارد' : print 'ندارد'; ?></span>
-                        </div>
+                        <div class="card-options card-row">
+                            <div class="sqfr option">
+                                <h1>متراژ</h1>
+                                <span><?= $post->area ?></span> متر
+                            </div>
+                            <div class="beds option">
+                                <h1>اتاق</h1>
+                                <span><?= $post->rooms ?></span>
+                            </div>
+                            <div class="bath option">
+                                <h1>حمام</h1>
+                                <span><?= $post->shower ?></span>
+                            </div>
+                            <div class="bath option">
+                                <h1>آدرس</h1>
+                                <span><?= $post->address ?></span>
+                            </div>
+                            <div class="balcony option">
+                                <h1>بالکن</h1>
+                                <span><?php $post->balcony == 10 ? print 'دارد' : print 'ندارد'; ?></span>
+                            </div>
+                            <div class="parking option">
+                                <h1>پارکینگ</h1>
+                                <span><?php $post->parking == 10 ? print 'دارد' : print 'ندارد'; ?></span>
+                            </div>
 
+                        </div>
+                        <div class="card-price card-row">
+                            تومان <h1><?= $post->price ?></h1>
+                        </div>
                     </div>
-                    <div class="card-price card-row">
-                        تومان <h1><?= $post->price ?></h1>
-                    </div>
-                </div>
-            </a>
+                </a>
             <?php } ?>
-        </div> 
+        </div>
     </section>
 
 
     <section class="category-list">
 
-    <div class="category-list-header">
-    <?php 
-                            global $pdo;
-                            $query = "SELECT * FROM `categories`";
-                            $statement = $pdo->prepare($query);
-                            $statement->execute();
-                            $categories = $statement->fetchAll();
-                            foreach($categories as $category){
-                            ?>
-            <div class="item click selected">
-                <a href=<?= url('category.php?cat_id='. $category->id) ?>'selected'><?= $category->name ?></a>
-            </div>
+    <h1 class="section-header">دسته بندی ها</h1>
+
+        <div class="category-list-header">
+            <?php
+            global $pdo;
+            $query = "SELECT * FROM `categories`";
+            $statement = $pdo->prepare($query);
+            $statement->execute();
+            $categories = $statement->fetchAll();
+            foreach ($categories as $category) {
+            ?>
+                <div class="item click selected">
+                    <a href=<?= url('category.php?cat_id=' . $category->id) ?>'selected'><?= $category->name ?></a>
+                </div>
             <?php } ?>
         </div>
-     
+
     </section>
+
+
+    <!-- Footer -->
+    <footer>
+
+
+
+        <!-- Logo & Social Media (لوگو و فضای مجازی) -->
+        <div class="logo-social-media">
+
+            <div class="footer-background">
+                <img src="/images/logo.png" alt="">
+            </div>
+
+            <p class="logo">Fire Watch</p>
+
+            <div class="social-media">
+                <ul>
+                    <li>
+                        <i class="fa-brands fa-instagram"></i>
+
+                    </li>
+                    <li>
+                        <i class="fa-brands fa-whatsapp"></i>
+
+                    </li>
+                    <li>
+                        <i class="fa-brands fa-telegram"></i>
+
+
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+
+        <!-- Customer Support (خدمات مشتریان) -->
+        <div class="footer-menu">
+
+
+            <h1 class="footer-list-header">خدمات مشتریان
+                <i class="fa-solid fa-user"></i>
+            </h1>
+
+
+            <ul class="list">
+                <li>
+                    <a href="#">
+                        <p>قوانین</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <p>پشتیبانی</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <p>خرید اشتراک</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <p>تمدید اشتراک</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <p>راهنمای سایت</p>
+                    </a>
+                </li>
+            </ul>
+
+
+
+
+        </div>
+
+        <!-- Connection Ways (راه های ارتباطی) -->
+        <div class="footer-menu">
+
+
+
+
+            <h1 class="footer-list-header">راه های ارتباطی
+                <i class="fa-solid fa-phone-flip"></i>
+            </h1>
+            <ul class="list">
+                <li>
+                    <a href="#">
+                        <p> : آدرس</p>
+                        <span>اصفهان ،اصفهان</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <p> : شماره تماس </p>
+                        <span>۳۱۲۳۴۵۷۹-۰۳۱</span>
+
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <p>: ایمیل پشتیبانی</p>
+                        <span>Firewatch 125@gmail.com</span>
+
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <p></p>
+                    </a>
+
+
+
+                </li>
+
+
+            </ul>
+
+
+        </div>
+
+        <!-- About us (درباره ی ما) -->
+        <div class="footer-menu">
+
+
+            <h1 class="footer-list-header">درباره ی ما
+                <i class="fa-solid fa-circle-info"></i>
+
+            </h1>
+            <ul class="list">
+                <li>
+                    <a href="#">
+                        <p>فایرواچ،شرکتی مجهز به فناوری های نوین در حوزه حریق و دارای حسگرهای مجهز به هوش مصنوعی و سیستم
+                            نظارتی ۲۴ ساعت هست که فعالیت خود را از سال ۱۴۰۱ با همکاری سازمان آتش نشانی ایران و شرکت
+                            سنسورتک آغاز کرده است</p>
+                    </a>
+                </li>
+                <li class="special">
+                    <a href="#"><img src="/images/footer/1.png" alt=""></a>
+                    <a href="#"><img src="/images/footer/2.png" alt=""></a>
+                    <a href="#"><img src="/images/footer/3.png" alt=""></a>
+                </li>
+
+            </ul>
+
+
+        </div>
+
+
+    </footer>
+
 
 
     <!-- Jquery And Slick Slider -->
@@ -164,8 +318,9 @@
 
     <!-- Main Page Slider Options -->
     <script src=<?= asset('assets/js/main-page/slider.js') ?>></script>
-    
-    
+
+
 
 </body>
+
 </html>
--- panel/post/index.php
+++ panel/post/index.php
@@ -1,7 +1,7 @@
 <?php
- require_once '../../functions/helpers.php'; 
- require_once '../../functions/pdo_connection.php'; 
- require_once '../../functions/check-login.php';
+require_once '../../functions/helpers.php';
+require_once '../../functions/pdo_connection.php';
+require_once '../../functions/check-login.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,15 +10,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>panel posts</title>
-    <link rel="stylesheet" href=<?= asset('assets/css/bootstrap/bootstrap.min.css')?> media="all" type="text/css">
-    <link rel="stylesheet" href=<?= asset('assets/css/admin-panel/admin.panel.css.map')?> media="all" type="text/css">
-    <link rel="stylesheet" href=<?= asset('assets/css/admin-panel/admin.panel.css')?> media="all" type="text/css">
-   
+    <link rel="stylesheet" href=<?= asset('assets/css/bootstrap/bootstrap.min.css') ?> media="all" type="text/css">
+    <link rel="stylesheet" href=<?= asset('assets/css/admin-panel/admin.panel.css.map') ?> media="all" type="text/css">
+    <link rel="stylesheet" href=<?= asset('assets/css/admin-panel/admin.panel.css') ?> media="all" type="text/css">
+
 </head>
 
 <body>
-<?php require_once '../layouts/top-nav.php'; ?>
-<?php require_once '../layouts/sidebar.php'; ?>
+    <?php require_once '../layouts/top-nav.php'; ?>
+    <?php require_once '../layouts/sidebar.php'; ?>
     <section class="app" id="app">
 
         <section class="container-fluid">
@@ -27,7 +27,7 @@
 
                     <section class="table-container-header">
                         <h1>پست ها</h1>
-                        <a href="<?=url('panel/post/create.php'); ?>" class="click">ایجاد پست جدید</a>
+                        <a href="<?= url('panel/post/create.php'); ?>" class="click">ایجاد پست جدید</a>
                     </section>
 
                     <section class="table-container-body">
@@ -51,70 +51,71 @@
                                 </tr>
                             </thead>
 
-                       <?php
-                    global $pdo;
-                    $query = "SELECT `posts`.*, categories.name AS category_name From posts LEFT JOIN categories ON posts.cat_id = categories.id";
-                    $statement = $pdo->prepare($query);
-                    $statement ->execute();
-                    $posts = $statement->fetchall();
-                    foreach($posts as $post){
-                       ?>
-                            <tbody>
-                                <tr>
-                                    <td><?= $post->id ?></td>
-                                    <td class="image"><img src="<?= asset($post->image)?>" alt=""></td>
-                                    <td><?= $post->title ?></td>
-                                    <td><?= $post->category_name ?></td>
-                                    <td>
-                                        <p><?= $post->body ?></p>
-                                    </td>
-                                    <td><?= $post->rooms ?></td>
-                                    <td><?php 
-                                    if($post->balcony == 10){
-                                        echo 'دارد';}
-                                        else{
-                                            echo 'ندارد';
-                                        }
-                                     ?></td>
-                                    <td><?= $post->area ?></td>
-                                    <td><?= $post->shower ?>
+                            <?php
+                            global $pdo;
+                            $query = "SELECT `posts`.*, categories.name AS category_name From posts LEFT JOIN categories ON posts.cat_id = categories.id";
+                            $statement = $pdo->prepare($query);
+                            $statement->execute();
+                            $posts = $statement->fetchall();
+                            foreach ($posts as $post) {
+                            ?>
+                                <tbody>
+                                    <tr>
+                                        <td><?= $post->id ?></td>
+                                        <td class="image"><img src="<?= asset($post->image) ?>" alt=""></td>
+                                        <td><?= $post->title ?></td>
+                                        <td><?= $post->category_name ?></td>
+                                        <td>
+                                            <p><?= $post->body ?></p>
                                         </td>
-                                    <td><?php if($post->parking == 10){
-                                        echo 'دارد';}
-                                        else{
-                                            echo 'ندارد';
-                                        }?></td>
-                                    <td><?= $post->price ?></td>
-                                    <td>
-                                        <p><?= $post->address ?></p>
-                                    </td>
-                                    <td>
-                                        <p><?php if($post->status == 10){
-                                            echo 'فعال';
-                                        } 
-                                        else {echo 'غیر فعال';}?></p>
-                                    </td>
+                                        <td><?= $post->rooms ?></td>
+                                        <td><?php
+                                            if ($post->balcony == 10) {
+                                                echo 'دارد';
+                                            } else {
+                                                echo 'ندارد';
+                                            }
+                                            ?></td>
+                                        <td><?= $post->area ?></td>
+                                        <td><?= $post->shower ?>
+                                        </td>
+                                        <td><?php if ($post->parking == 10) {
+                                                echo 'دارد';
+                                            } else {
+                                                echo 'ندارد';
+                                            } ?></td>
+                                        <td><?= $post->price ?></td>
+                                        <td>
+                                            <p><?= $post->address ?></p>
+                                        </td>
+                                        <td>
+                                            <p><?php if ($post->status == 10) {
+                                                    echo 'فعال';
+                                                } else {
+                                                    echo 'غیر فعال';
+                                                } ?></p>
+                                        </td>
 
-                                    <td class="buttons">
-                                             <a href="<?= url('panel/post/edit.php?post_id=' . $post->id);?>" class="btn btn-info btn-sm">ویرایش</a>
-                                                <a href="<?= url('panel/post/delete.php?post_id='. $post->id);?>" class="btn btn-danger btn-sm">حذف</a>
-                                                <a href="<?= url('panel/post/change-status.php?post_id='. $post->id);?>" class="btn btn-warning btn-sm">وضعیت</a>
-                                               
-                                        </form>
-                                    </td>
-                                </tr>
-                            </tbody>
+                                        <td class="buttons">
+                                            <form>
+                                            <a href="<?= url('panel/post/edit.php?post_id=' . $post->id); ?>" class="btn btn-info btn-sm">ویرایش</a>
+                                            <a href="<?= url('panel/post/delete.php?post_id=' . $post->id); ?>" class="btn btn-danger btn-sm">حذف</a>
+                                            <a href="<?= url('panel/post/change-status.php?post_id=' . $post->id); ?>" class="btn btn-warning btn-sm">وضعیت</a>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                </tbody>
                             <?php } ?>
 
                         </table>
-                       </section>
+                    </section>
                 </section>
             </section>
         </section>
 
     </section>
-   <script src="https://code.iconify.design/2/2.2.1/iconify.min.js"></script>
-   <script src="<?= asset('assets/js/bootstrap.min.js'); ?>"></script>
+    <script src="https://code.iconify.design/2/2.2.1/iconify.min.js"></script>
+    <script src="<?= asset('assets/js/bootstrap.min.js'); ?>"></script>
     <script src="<?= asset('assets/js/jquery.min.js'); ?>"></script>
 </body>
 
